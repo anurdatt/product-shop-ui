0b80cd4cc44b49a416e2dd056af2cb4560bc2e34
Anuran Datta <anurdatt@in.ibm.com>
2020-02-02 12:46:46 +0530

    add search by image

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -5,7 +5,8 @@ var productRoutes = require('express').Router(),
     fs = require('fs'),
     multer = require('multer'),
     jimp = require('jimp'),
-    pm = require('pixelmatch');
+    PNG = require('pngjs').PNG;
+    pixelmatch = require('pixelmatch');
 
 var serviceUrl = process.env.PRODUCT_API_BE_SERVICE_URL || process.env.OPENSHIFT_PRODUCT_API_BE_SERVICE_URL 
                         || "http://product-api-sb-cloudstar.inmbzp8022.in.dst.ibm.com/api/v1/product";
@@ -380,19 +381,29 @@ productRoutes.post('/SearchProductsByImage', function(req, res) {
 
                 // elem['imageUrl'] = endpoint + '/' + bucketP + '/' + elem.itemId + '.jpg';
                 
-                getObjectData(bucketP, `${elem.itemId}.jpg`, function(error, data) {
+                getObjectData(bucketP, `${elem.itemId}.jpg`, async function(error, data) {
                     if (error || data == null) {
                         //throw error if reqd.
-                        console.error('Error getting Object with Id ' + elem.itemId + ' from COS');
+                        console.error('Error getting Object with Id ' + elem.itemId + ' from COS -',error );
                     }
                     else {
                         console.log('Got Object with Id ' + elem.itemId + ' from COS');
                         elem['imageUrl'] = endpoint + '/' + bucketP + '/' + elem.itemId + '.jpg';
                         
-                        const match = matchImageData(searchImg, Buffer.from(data.Body)); //returns true/false
-                        if (match) {
+                        // var matchResult = matchImageData(searchImg, Buffer.from(data.Body)); //returns true/false
+                        // console.log('matchResult =', matchResult);
+                        // if (matchResult && matchResult!=undefined &&
+                        //     matchResult.status == 'PASS' && matchResult.match) {
+                        //     finalResultJSON.push(elem);
+                        // }
+
+                        var matchResult = await matchImageDataAsync(searchImg, Buffer.from(data.Body)); //returns true/false
+                        console.log('matchResult =', matchResult);
+                        if (matchResult && matchResult != undefined &&
+                            matchResult.status == 'PASS' && matchResult.match == true) {
                             finalResultJSON.push(elem);
                         }
+                        
                     }
                     //console.log(JSON.stringify(elem));
                     next();
@@ -428,49 +439,198 @@ function getObjectData(bucket, itemKey, cb) {
     });
 }
 
-async function asyncGetObjectData(bucket, itemKey) {
+/* async function asyncGetObjectData(bucket, itemKey) {
     console.log(`Retrieving item from bucket: ${bucket}, key: ${itemKey}`);
     return cosClient.getObject({
         Bucket: bucket, 
         Key: itemKey
     }).promise();
 }
-
-function matchImageData(searchBuffer, itemBuffer) {
+ */
+/* function matchImageData(searchBuffer, itemBuffer) {
     //TODO
     console.log(searchBuffer);
     console.log('searchBuffer.length = ' + searchBuffer.length);
     console.log(itemBuffer);
     console.log('itemBuffer.length = ' + itemBuffer.length);
-    return true;
-    /* async.waterfall([
+    //return true;
+    async.waterfall([
         function(next) {
             jimp.read(searchBuffer)
             .then( simage => {
-                simage.resize(253, 182);
+                simage.resize(253, 182)
+                .quality(60);
                 //console.log('resized search image Object fields = ' + JSON.stringify(Object.keys(simage)));
-                console.log('resized search image data length = ' + simage.getBuffer().length);
-                next(null, simage);
+
+                jimp.read(itemBuffer)
+                .then( iimage => {
+                    iimage.resize(253, 182)
+                    .quality(60);
+                    //console.log('resized item image data length = ' + iimage.getBuffer().length);
+                    next(null, simage, iimage);
+                })
+                .catch(err => {
+                    next(err)
+                });
+
+                //next(null, simage);
             })
-        },
-        function(res1, next) {
-            jimp.read(itemImage)
-            .then( iimage => {
-                iimage.resize(253, 182);
-                console.log('resized item image data length = ' + iimage.getBuffer().length);
-                next(null, res1, iimage);
-            })
+            .catch(err => {
+                next(err)
+            });
         },
         function(res1, res2, next) {
-            pm.pixel
+            //console.log('Inside buffer cb', res1, res2);
+            res1.getBufferAsync(res1.getMIME())
+            .then( simageBuffer => {
+                //console.log('simageBuffer = ', simageBuffer);
+                res2.getBufferAsync(res2.getMIME())
+                .then ( iimageBuffer => {
+                    //console.log('iimageBuffer = ', iimageBuffer);
+                    next(null, simageBuffer, iimageBuffer);
+                })
+                .catch(err => {
+                    next(err)
+                });
+            })
+            .catch(err => {
+                next(err)
+            });
+        },
+        function (res1, res2, next) {
+            var diff;
+            var ret;
+            
+            try {
+                const pngs=PNG.sync.read(res1);
+                const pngi=PNG.sync.read(res2);
+                //console.log ('Inside async last cb, ',  pngs.data, pngi.data );
+                diff = new PNG({ width: pngs.width, height: pngs.height})
+                ret = pixelmatch(pngs.data, pngi.data, diff.data, pngs.width, pngs.height, {threshold:0.1});
+            }
+            catch(e) {
+                next(e);
+                return;
+            }
+            if (ret == null) {
+                next('Failed - no return value from pixelmatch');
+                return;
+            }
+
+            next(null, { ret: ret, diff: diff });
         }
 
     ], function(err, result) {
+        var match = false; 
+        if (err) {
+            console.error('FATAL ERROR in matchImageData - ', err);
+            return {status: "FAIL", match: match};
+        }
+        // TODO - calculate image diff result
+        //console.log('result.ret = ', result.ret);
+        if (result.ret == 0 ) { match = true; }
 
-    }); */
-
+        return {status: "PASS", match: match};
+        
+    });
     
+    
+}
+ */
 
+function matchImageDataAsync(searchBuffer, itemBuffer) {
+    //TODO
+    console.log(searchBuffer);
+    console.log('searchBuffer.length = ' + searchBuffer.length);
+    console.log(itemBuffer);
+    console.log('itemBuffer.length = ' + itemBuffer.length);
+    //return true;
+    return new Promise( resolve => {
+
+
+        async.waterfall([
+            function(next) {
+                jimp.read(searchBuffer)
+                .then( simage => {
+                    simage.resize(253, 182)
+                    .quality(60);
+                    //console.log('resized search image Object fields = ' + JSON.stringify(Object.keys(simage)));
+    
+                    jimp.read(itemBuffer)
+                    .then( iimage => {
+                        iimage.resize(253, 182)
+                        .quality(60);
+                        //console.log('resized item image data length = ' + iimage.getBuffer().length);
+                        next(null, simage, iimage);
+                    })
+                    .catch(err => {
+                        next(err);
+                    });
+    
+                    //next(null, simage);
+                })
+                .catch(err => {
+                    next(err);
+                });
+            },
+            function(res1, res2, next) {
+                //console.log('Inside buffer cb', res1, res2);
+                res1.getBufferAsync(res1.getMIME())
+                .then( simageBuffer => {
+                    //console.log('simageBuffer = ', simageBuffer);
+                    res2.getBufferAsync(res2.getMIME())
+                    .then ( iimageBuffer => {
+                        //console.log('iimageBuffer = ', iimageBuffer);
+                        next(null, simageBuffer, iimageBuffer);
+                    })
+                    .catch(err => {
+                        next(err);
+                    });
+                })
+                .catch(err => {
+                    next(err);
+                });
+            },
+            function (res1, res2, next) {
+                var diff;
+                var ret;
+                
+                try {
+                    const pngs=PNG.sync.read(res1);
+                    const pngi=PNG.sync.read(res2);
+                    //console.log ('Inside async last cb, ',  pngs.data, pngi.data );
+                    diff = new PNG({ width: pngs.width, height: pngs.height})
+                    ret = pixelmatch(pngs.data, pngi.data, diff.data, pngs.width, pngs.height, {threshold:0.1});
+                }
+                catch(e) {
+                    next(e);
+                    return;
+                }
+                if (ret == null) {
+                    next('Failed - no return value from pixelmatch');
+                    return;
+                }
+    
+                next(null, { ret: ret, diff: diff });
+            }
+    
+        ], function(err, result) {
+            var match = false; 
+            if (err) {
+                console.error('FATAL ERROR in matchImageData - ', err);
+                resolve({status: "FAIL", match: match});
+                return;
+            }
+            // TODO - calculate image diff result
+            //console.log('result.ret = ', result.ret);
+            if (result.ret == 0 ) { match = true; }
+    
+            resolve({status: "PASS", match: match});
+            
+        });
+        
+    });
+    
     
 }
 
